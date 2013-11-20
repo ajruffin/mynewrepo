445ed41b84d14c3fb5f0ece444f31fe97f239d18
AJ Ruffin <ajruffin711@gmail.com>
2013-11-20 06:46:01 +0000

    fixed typos, end of lesson 8 video

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def show
     @business = Business.find_by_name(params[:id])
     if @business
-      @statuses = @business.statuses.all
+      @statuses = @business add.statuses.all
       render action: :show
     else
       render file: 'public/404', status: 404, formats: [:html]
